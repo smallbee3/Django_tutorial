eeda1e0619fd50183e7dd4e8e8c26a818e03c19b
smallbee <smallbee3@gmail.com>
2018-02-02 14:18:59 +0900

    Choice, Question을 admin에 등록

--- django/polls/admin.py
+++ django/polls/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+# from django.polls.models import Question, Choice
+# django라는 이름의 package가 있어서 못찾는 것.
+
+from .models import Question, Choice
+
+
+
+admin.site.register(Question)
+admin.site.register(Choice)
+
